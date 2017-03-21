6bc2d6f0b7d734c35388c1a6adddb0367a10fdd4
Leandro <Leandro@DESKTOP-VM6220G>
2017-03-20 23:20:43 -0300

    Criado Repositorio.

---
+++ src/testepi1/TestePI1.java
@@ -0,0 +1,20 @@
+package testepi1;
+
+import java.util.Scanner;
+
+/*
+Receba dois números e mostre o produto deles.
+ */
+public class TestePI1 {
+
+    public static void main(String[] args) {
+        double NUM1, NUM2;
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Escreva o primeiro número:");
+        NUM1 = leitor.nextDouble();
+        System.out.println("Escreva o segundo número");
+    }
+    
+}
